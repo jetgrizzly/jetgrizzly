da1d81e489798acb8b728df08ca2b05606fc5ebc
jgladch <jeffgladchun@gmail.com>
2014-10-16 11:52:02 -0700

    (feat) Fix broken tests for niave queue implementation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,19 @@
  * Controller of the jetgrizzlyApp
  */
 angular.module('jetgrizzlyApp')
-  .controller('QueueController', function ($scope) {
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+angular.module('jetgrizzlyApp')
+  .controller('VideoQueueController', function ($scope) {
     $scope.userQueue = [];
     $scope.addToQueue = function(item) {
       $scope.userQueue.push(item);
     };
   });
+
