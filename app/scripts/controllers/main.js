bd598504ac567ce36803a62300a96530419e870a
jgladch <jeffgladchun@gmail.com>
2014-10-16 10:51:26 -0700

    Implement niave user queue, refactor index view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,9 @@
  * Controller of the jetgrizzlyApp
  */
 angular.module('jetgrizzlyApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('QueueController', function ($scope) {
+    $scope.userQueue = [];
+    $scope.addToQueue = function(item) {
+      $scope.userQueue.push(item);
+    };
   });
