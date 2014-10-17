5e80152223d2ea7971f673d014ff2bfac23ec478
moxizhou <mzhou8@u.rochester.edu>
2014-10-16 17:09:37 -0700

    (fix) Fixed merge errors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,19 +26,10 @@ angular.module('jetgrizzlyApp', [])
   $scope.queueSong = function(){
     playerFactory.playSong($scope.songUrl);
   };
-
 })
-
-
 
 .factory('playerFactory', function() {
   //queueSong function from controller calls this 
-  var playSong = function(song) {
-  }
-
-  return {
-    playerSong: playSong
-  }
 })
 
 
@@ -47,14 +38,14 @@ angular.module('jetgrizzlyApp', [])
 
   return {
     //elements attribute settings i.e. id, height attrs
-    restrict: "E",
+    restrict: 'E',
 
     scope: {
       //bind attrs to our directive scope
       //one way binding - data changed in the view is updated in javascript
-      height: "@",
-      width: "@",
-      videoid: "@"
+      height: '@',
+      width: '@',
+      videoid: '@'
     },
 
     //template to put inside of directive
@@ -63,14 +54,14 @@ angular.module('jetgrizzlyApp', [])
     link: function(scope, element) {
       //Load the iFrame player API code asynchronously
       var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
+      tag.src = 'https://www.youtube.com/iframe_api';
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
       var player;
 
       $window.onYouTubeIframeAPIReady = function() {
-        player = new YT.Player(element.children()[0], {
+        player = new window.YT.Player(element.children()[0], {
           playerVars: {
             autoplay: 1,
             html5: 1,
@@ -80,11 +71,11 @@ angular.module('jetgrizzlyApp', [])
           width: scope.width,
           videoId: scope.videoid, 
         });
-      }
+      };
 
       //If a change is made to videoid via input box in view, this will see the change and runs callback
       scope.$watch('videoid', function(newValue, oldValue) {
-        if (newValue == oldValue) {
+        if (newValue === oldValue) {
           return;
         }
         //YouTube will instantly change the currently playing video
