89709e2161de1d5b713128b7b75c0ce0a163726d
Jose Joaquin <jomerinog@gmail.com>
2014-10-18 00:29:16 -0700

    attempt to fix heroku procfile

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var ip = process.env.IP  || undefined;
+
+// Serve static files from client/ . This file is copied to dist in production.
 app.use(express.static(__dirname + '/client'));
+
 var server = require('http').createServer(app);
 // Start server
 server.listen(port, ip, function () {
+  console.log('Asset dir is: '+ config.assetsDir);
   console.log('Express server listening on %d!', port);
 });
 
