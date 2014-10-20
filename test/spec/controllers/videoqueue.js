48cfd83fb3b7d99c979d44a69a6a32d9e96cb6f6
jgladch <jeffgladchun@gmail.com>
2014-10-20 16:31:36 -0700

    (fix) Add tests for queue feature

--- test/spec/controllers/videoqueue.js
+++ test/spec/controllers/videoqueue.js
@@ -17,6 +17,7 @@ describe('Controller: VideoqueueCtrl', function () {
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(undefined);
+    expect(scope.addToQueue).not.toBe(undefined);
+    expect(scope.removeFirst).not.toBe(undefined);
   });
 });
