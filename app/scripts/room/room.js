4353fdda93f2ec4fcf3e0963c89904779173a45a
Alexander Jeng <alexanderjeng@gmail.com>
2014-10-22 09:42:35 -0700

    Changed video queue in room.js

--- app/scripts/room/room.js
+++ app/scripts/room/room.js
@@ -1,7 +1,5 @@
 'use strict';
-(function(){
-
-var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function ($stateProvider) {
+module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function ($stateProvider) {
   $stateProvider.state('lobby', {
     url: '/',
     parent: 'app',
@@ -17,7 +15,7 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
     width: 640,
     height: 390
    };
-
+  
 
   //Link up with songqueue, insert new property of videoID
   // i.e. videoID: song.split('=')[1]
@@ -28,7 +26,7 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
 }).factory('playerFactory', function () {
   //queueSong function from controller calls this
 })
-  .directive('youtube', function ($window, config) {
+  .directive('youtube', function ($rootScope, $window, config, $firebase) {
     return {
       //elements attribute settings i.e. id, height attrs
       restrict: 'E',
@@ -37,18 +35,25 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
         //one way binding - data changed in the view is updated in javascript
         height: '@',
         width: '@',
-        videoid: '@',
+        videoid: '@'        
       },
+
       //template to put inside of directive
       template: '<div></div>',
       link: function (scope, element) {
         //Load the iFrame player API code asynchronously
-        var tag = element.append('<script src="https://www.youtube.com/iframe_api">');
+        var tag = document.createElement('script');
+        tag.src = 'https://www.youtube.com/iframe_api';
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
         var player;
 
+
         $window.onYouTubeIframeAPIReady = function () {
-          var isPlaying;
-          var youTubeFirebase = new $window.Firebase('https://blistering-heat-6745.firebaseio.com/youTube');
+          var isPlaying
+          var youTubeFirebase = new $window.Firebase(config.firebase.url+'/youTube');
+          var videoFirebase = new $window.Firebase(config.firebase.url+'/youTube/currentVideo')
 
           youTubeFirebase.once('value', function(snap){
             var youtubeInfo = snap.val();
@@ -62,9 +67,7 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
               console.log(startTime);
               console.log(currentVideo);
 
-              console.log(element.children()[0]);
-
-              player = new $window.YT.Player(element.children()[0], {
+              player = new window.YT.Player(element.children()[0], {
                 playerVars: {
                   autoplay: 1,
                   html5: 1,
@@ -74,12 +77,22 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
                 height: scope.height,
                 width: scope.width,
                 videoId: currentVideo,
+                events: {
+                  'onStateChange': function(event){
+                    console.log("youtube player has a stateChange");
+                    console.log("Event data ", event.data);
+                    if(event.data === 1){
+                      console.log("youtube player video ended")
+                      $rootScope.$broadcast('videoEnded');
+                    }
+                  }
+                }
                // startSeconds: startTime
              });
               // player.loadVideoById({'videoId': currentVideo, 'startSeconds': startTime });
             }
             else {
-              player = new $window.YT.Player(element.children()[0], {
+              player = new window.YT.Player(element.children()[0], {
                 playerVars: {
                   autoplay: 0,
                   html5: 1,
@@ -88,34 +101,44 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
                 height: scope.height,
                 width: scope.width,
                 videoId: scope.videoid,
-              });
+                events: {
+                  'onStateChange': function(event){
+                    console.log("youtube player has a stateChange");
+                    console.log("Event data ", event.data);
+                    if(event.data === 1){
+                      console.log("youtube player video ended")
+                      $rootScope.$broadcast('videoEnded');
+                    }
+                  }
+                }
+              });       
             }
           });
-
+          
+          //If a change is made to videoid via input box in view, this will see the change and runs callback
+          videoFirebase.on('child_changed', function(childSnapshot, prevChildName){
+             console.log('firebase value changed');
+             youTubeFirebase.child('startTime').set(Date.now());
+             youTubeFirebase.child('isPlaying').set(true);
+             youTubeFirebase.child('currentVideo').set(childSnapshot);
+             console.log('childSnapshot', childSnapshot)
+             player.loadVideoById({'videoId': childSnapshot.val()}); 
+          });
         };
 
-
-        if($window.YT !== undefined){
-          $window.onYouTubeIframeAPIReady();
-        }
-        //If a change is made to videoid via input box in view, this will see the change and runs callback
+        //$rootScope.$broadcast('onOnlineUser'); NEED TO USE LATER
         scope.$watch('videoid', function (newValue, oldValue) {
 
 
           if (newValue !== oldValue) {
-            console.log('setting new video value');
-            var firebase = new $window.Firebase(config.firebase.url+'/youTube');
+            console.log("setting new video value")
+            var firebase = new Firebase(config.firebase.url+'/youTube');
             firebase.child('startTime').set(Date.now());
             firebase.child('isPlaying').set(true);
             firebase.child('currentVideo').set(newValue);
 
-            var currentVideo = new $window.Firebase('https://blistering-heat-6745.firebaseio.com/youTube');
-            currentVideo.once('value', function(snap){
-              var obj = snap.val();
-              console.log(obj.currentVideo);
-            });
-
-
+        
+            
           }
 
           //TODO MAKE A LISTENER FOR FIREBASE CHANGE
@@ -127,4 +150,4 @@ var module = angular.module('jetgrizzlyApp.Room', ['ui.router']).config(function
       }
     };
 });
-})();
+
