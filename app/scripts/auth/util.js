22e605c4c15e56ca8f18bb97f4f8cebfeb01a636
moxizhou <mzhou8@u.rochester.edu>
2014-10-23 15:58:51 -0700

    (fix)jshints

--- app/scripts/auth/util.js
+++ app/scripts/auth/util.js
@@ -1,11 +1,12 @@
+'use strict';
 angular.module('firebase.utils', ['firebase'])
 .factory('fbutil', function($window, $firebase){
+  function firebaseRef() {
+    var ref = new $window.Firebase('https://blistering-heat-6745.firebaseio.com');
+    return ref;
+  }
 	return {
 		ref: firebaseRef
 	};
 
-	function firebaseRef() {
-		var ref = new $window.Firebase('https://blistering-heat-6745.firebaseio.com');
-		return ref;
-	}
-})
+});
