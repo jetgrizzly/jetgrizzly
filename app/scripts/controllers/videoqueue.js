91c806fade997d40edeb59a62d5b26f280793c2c
jgladch <jeffgladchun@gmail.com>
2014-10-23 19:28:53 -0700

    (feat) Move Base Case Queue Logic Into Server

--- app/scripts/controllers/videoqueue.js
+++ app/scripts/controllers/videoqueue.js
@@ -27,38 +27,11 @@
         });
       });
 
-      $scope.$on('videoEnded', function() {
-        $scope.$apply(function() {
-          console.log('Video Queue CTRL has heard videoEnded Event');
-          if ($scope.queue.length > 0) {
-            var newVid = $scope.queue[0].$value.split('v=')[1];
-            console.log('Newvid is: ', newVid);
-            $scope.removeFirst();
-            videoRef.child('currentVideo').set(newVid);
-          } else {
-            console.log('Video Queue CTRL: There are no videos to play.');
-          }
-        });
-      });
-
       $scope.addToQueue = function(item) {
         console.log('Link added: '+item);
         $scope.queue.$add(item).then(function(){
           console.log('Queue size: '+$scope.queue.length+'; Player is in state: '+$scope.playerState);
-          //The if statement below never gets triggered because playerState never
-          //gets set to 0 somehow... or it gets set back to 1 insanely quickly...
-          if ($scope.queue.length === 1 && ($scope.playerState === 0 || $scope.playerState === undefined)) {
-            var newVid = $scope.queue[0].$value.split('v=')[1];
-            console.log('Newvid is: ', newVid);
-            videoRef.child('currentVideo').set(newVid);
-            $scope.removeFirst();
-          }
         });
-
-        //This function needs to know: 1) If there is a video currently playing, 2) The current size of the queue
-        //If queue.length > 0, push item into queue
-        //If queue.length === 0 && video is playing, push item into queue
-        //If queue.length === 0 && video is not playing, change currentVideo in firebase and start playing video right away
       };
 
       $scope.removeFirst = function() {
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var ip = process.env.IP  || undefined;
+var Firebase = require("firebase");
 
 // Serve static files from client/ . This file is copied to dist in production.
 app.use(express.static(__dirname + '/client'));
@@ -10,6 +11,56 @@ var server = require('http').createServer(app);
 // Start server
 server.listen(port, ip, function () {
   console.log('Express server listening on %d!', port);
+});
+
+var videoSet = false;
+var queueRef = new Firebase('https://blistering-heat-6745.firebaseio.com/queue/');
+var videoRef = new Firebase('https://blistering-heat-6745.firebaseio.com/youTube/');
+//Will use beenCalled to throttle the number of calls to this listener
+var beenCalled = false;
+//Listen for changes to player state from firebase
+videoRef.on('value', function (snapshot) {
+  //Print whether this callback chain has already been called for the value change
+  console.log('beenCalled: ', beenCalled);
+  //If this is the first time this has been called
+  if (!beenCalled) {
+    beenCalled = true;
+    //Check if the player is playing, print it
+    var isPlaying = snapshot.val().isPlaying;
+    console.log('isPlaying: ',isPlaying);
+    //If player is not playing
+    if (!isPlaying) {
+      //Check the queue
+      queueRef.once('value', function(queue) {
+        console.log('queue: ', queue.val());
+        //If the queue is not empty
+        if (queue.val() !== null) {
+          //Get the queue in array form
+          var index = 0;
+          var videoQueue = [];
+          queue.forEach(function(queueEntry){
+            videoQueue[index] = queueEntry.val();
+            index ++;
+          });
+          //Parse the first link
+          nextVid = videoQueue[0].split('v=')[1];
+          console.log('next vid is: ', nextVid);
+          //Set the currentVideo in firebase to the first item in queue s
+          var currentVideo = videoRef.child('currentVideo');
+          currentVideo.set(nextVid, function(){
+            console.log("next vid saved");
+            //Once next video has been sent, change beenCalled back to false
+            beenCalled = false;
+          });
+
+        }
+      });
+    } else {
+      beenCalled = false;
+    }
+  }
+}, function (errorObject) {
+  console.log('The read failed: ' + errorObject.code);
 });
 
 // Expose app
