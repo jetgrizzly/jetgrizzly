9068ead6279b6076b36af01b341222f23fe23a0f
moxizhou <mzhou8@u.rochester.edu>
2014-10-25 14:06:31 -0700

    (fix)style errors for chat.js'

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -1,5 +1,4 @@
 'use strict';
-
 angular.module('jetgrizzlyApp.chat', ['firebase'])
 	.controller('chatController', function($scope, $window, $firebase, $location, $anchorScroll, config) {
     var myChat = new $window.Firebase(config.firebase.url+'/messages');
@@ -7,15 +6,11 @@ angular.module('jetgrizzlyApp.chat', ['firebase'])
     //create an AngularFire reference to the data and download the data into a local object
     var sync = $firebase(limit);
     $scope.messages = sync.$asArray();
-
     //synchronize the object with 3-way data binding
     $scope.addMessage = function(user, text) {
       $scope.messages.$add({user: user, text: text});
       //supposed to set location to auto scroll to bottom??
       $location.hash('bottom');
-      // if ($scope.messages.length > 5) {
-      //   $scope.messages[0].remove;
-      // }
       $anchorScroll();
     };
   });
