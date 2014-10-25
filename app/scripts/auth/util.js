c81e5b5304612f520c51ea328efb7a6eb8a49d2e
moxizhou <mzhou8@u.rochester.edu>
2014-10-25 15:21:21 -0700

    (feat)basic unit test for chat controller

--- app/scripts/auth/util.js
+++ app/scripts/auth/util.js
@@ -5,10 +5,15 @@
 //and used for the same purpose of easier testing.
 angular.module('firebase.utils', ['firebase'])
 .factory('fbutil', function($window, $firebase){
-  function firebaseRef() {
-    var ref = new $window.Firebase('https://blistering-heat-6745.firebaseio.com');
-    return ref;
-  }
+  var firebaseRef = function(link) {
+  	if (link) {
+    	var ref = new $window.Firebase('https://blistering-heat-6745.firebaseio.com' + link);
+    	return ref;
+    } else {
+    	var ref = new $window.Firebase('https://blistering-heat-6745.firebaseio.com');
+  		return ref;
+  	}
+  };
 	return {
 		ref2: firebaseRef
 	};
--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -1,16 +1,17 @@
 'use strict';
-angular.module('jetgrizzlyApp.chat', ['firebase'])
-	.controller('chatController', function($scope, $window, $firebase, $location, $anchorScroll, config) {
-    var myChat = new $window.Firebase(config.firebase.url+'/messages');
-    var limit = myChat.limit(30);
+(function(){
+var module = angular.module('jetgrizzlyApp.chat', ['firebase.utils', 'firebase']);
+	module.controller('chatController', ['fbutil', '$scope', '$window', '$firebase', '$location', '$anchorScroll', function(fbutil, $scope, $window, $firebase, $location, $anchorScroll) {
+    var limit = fbutil.ref2('/messages').limit(30);
     //create an AngularFire reference to the data and download the data into a local object
     var sync = $firebase(limit);
     $scope.messages = sync.$asArray();
     //synchronize the object with 3-way data binding
     $scope.addMessage = function(user, text) {
       $scope.messages.$add({user: user, text: text});
-      //supposed to set location to auto scroll to bottom??
+      //set location to auto scroll to bottom
       $location.hash('bottom');
       $anchorScroll();
     };
-  });
+  }]);
+})();
