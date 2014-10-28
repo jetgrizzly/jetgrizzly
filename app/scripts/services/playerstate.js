3620caec4d7d61d14f993951f9b9246e256f9b5a
Liam Schauerman <ljschauerman@gmail.com>
2014-10-28 16:09:10 -0700

    style fix

--- app/scripts/services/playerstate.js
+++ app/scripts/services/playerstate.js
@@ -10,7 +10,10 @@
 angular.module('jetgrizzlyApp')
   .factory('playerState', function (config, $window, $q) {
     var youtubeRef = new $window.Firebase(config.firebase.url+'/youTube');
-    var currentVideoObject  = {isPlaying:false,currentVideo:""};
+    var currentVideoObject  = {
+      isPlaying:false,
+      currentVideo:""
+    };
 
     // video change is deferred so that player plays next when appropiate
     var deferredVideoChange = $q.defer();
