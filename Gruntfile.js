6dd4f9e78eaa9b86accdcbc3b263f4bdff7a087e
Liam Schauerman <ljschauerman@gmail.com>
2014-10-29 16:27:37 -0700

    remove test comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,6 @@
 // 'test/spec/{,*/}*.js'
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
-// test
 module.exports = function (grunt) {
 
   // Load grunt tasks automatically
